d923f656bb25862d737fb6e50d77754316e514f1
Akhilesh Kumar <rajakhil132@gmail.com>
2020-12-12 23:39:44 +0530

    setting wallpaper and xmobar on xmonad

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -1,9 +1,19 @@
 import XMonad
 import XMonad.Config.Desktop
+import XMonad.Hooks.DynamicLog
+import XMonad.Hooks.ManageDocks
+import XMonad.Util.Run(spawnPipe)
+import XMonad.Wallpaper
 
-main = xmonad desktopConfig
-   { terminal = "alacritty",
-     modMask = mod4Mask
-   }
+main = do
+   xmproc <- spawnPipe "xmobar"
+   setRandomWallpaper [ "$HOME/Wallpapers" ]
+   xmonad $docks desktopConfig
+      { layoutHook = avoidStruts $ layoutHook defaultConfig
+      , manageHook = manageHook def <+> manageDocks
+      , terminal = "alacritty"
+      , modMask = mod4Mask --Bind Mod to the Windows Key
+      , workspaces = myWorkspaces
+      }
      
-     
+myWorkspaces = [ "1", "2", "3", "4", "5" ]
