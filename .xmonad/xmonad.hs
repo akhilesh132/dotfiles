0d4641c8b37a1cc1dae52332b4eb5692c1333715
Akhilesh Kumar <rajakhil132@gmail.com>
2020-12-19 22:12:20 +0530

    Using smart border in xmonad layout

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -2,18 +2,19 @@ import XMonad
 import XMonad.Config.Desktop
 import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.ManageDocks
+import XMonad.Layout.NoBorders (noBorders, smartBorders)
 import XMonad.Util.Run(spawnPipe)
 import XMonad.Wallpaper
 
 main = do
-   xmproc <- spawnPipe "xmobar"
+   -- xmproc <- spawnPipe "xmobar"
    setRandomWallpaper [ "$HOME/Wallpapers" ]
    xmonad $docks desktopConfig
-      { layoutHook = avoidStruts $ layoutHook defaultConfig
+      { layoutHook = myLayoutHook
       , manageHook = manageHook def <+> manageDocks
       , terminal = "alacritty"
       , modMask = mod4Mask --Bind Mod to the Windows Key
       , workspaces = myWorkspaces
       }
-     
-myWorkspaces = [ "1", "2", "3", "4", "5" ]
+myLayoutHook = smartBorders $ avoidStruts $ layoutHook defaultConfig
+myWorkspaces = [ "1", "2", "3", "4", "5", "6", "7", "8", "9" ]
