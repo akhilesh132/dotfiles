94664e32d0a7247db37e4e6894d57b17a53bd3a4
Akhilesh Kumar <rajakhil132@gmail.com>
2020-12-21 22:01:44 +0530

    Add spacing around windows

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -3,18 +3,26 @@ import XMonad.Config.Desktop
 import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.ManageDocks
 import XMonad.Layout.NoBorders (noBorders, smartBorders)
+import XMonad.Layout.Spacing
 import XMonad.Util.Run(spawnPipe)
 import XMonad.Wallpaper
 
 main = do
-   xmproc <- spawnPipe "xmobar"
+   --xmproc <- spawnPipe "xmobar"
    setRandomWallpaper [ "$HOME/Wallpapers" ]
    xmonad $docks desktopConfig
       { layoutHook = myLayoutHook
       , manageHook = manageHook def <+> manageDocks
       , terminal = "alacritty"
-      , modMask = mod4Mask --Bind Mod to the Windows Key
+      , modMask = myModMask 
       , workspaces = myWorkspaces
       }
-myLayoutHook = smartBorders $ avoidStruts $ layoutHook defaultConfig
+
+myLayoutHook = spacingRaw True (Border 0 0 0 0) True (Border 0 0 0 0) True 
+               $ avoidStruts 
+               $ layoutHook defaultConfig
+
 myWorkspaces = [ "1", "2", "3", "4", "5", "6", "7", "8", "9" ]
+
+--Bind Mod to the Windows Key
+myModMask = mod4Mask
