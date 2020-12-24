8b98d2ec6e142287b39fe3e2d70537eaa341d51e
Akhilesh Kumar <rajakhil132@gmail.com>
2020-12-24 22:04:39 +0530

    Adding handleEvent hook along with minor adjustments

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -2,30 +2,43 @@ import XMonad
 import XMonad.Config.Desktop
 import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.ManageDocks
+import XMonad.Hooks.EwmhDesktops
+import XMonad.Hooks.ManageHelpers
 import XMonad.Layout.NoBorders (noBorders, smartBorders)
 import XMonad.Layout.Spacing
 import XMonad.Util.Run(spawnPipe)
 import XMonad.Wallpaper
 
 main = do
-   xmproc <- spawnPipe "xmobar"
+   --xmproc <- spawnPipe "xmobar"
    setRandomWallpaper [ "$HOME/Wallpapers" ]
    xmonad $docks desktopConfig
-      { layoutHook = myLayoutHook
-      , manageHook = manageHook def <+> manageDocks
+      { borderWidth = myBorderWidth
+      , layoutHook = myLayoutHook
+      , manageHook = myManageHook
+      , handleEventHook = myHandleEventHook 
       , terminal   = myTerminal
       , modMask    = myModMask 
       , workspaces = myWorkspaces
       }
 
-myLayoutHook = spacingRaw True (Border 0 5 5 5) True (Border 5 5 5 5) True 
-               $ smartBorders
-               $ avoidStruts 
-               $ layoutHook defaultConfig
+myBorderWidth = 1
 
-myWorkspaces = [ "1", "2", "3", "4", "5", "6", "7", "8", "9" ]
+myLayoutHook =  smartBorders $ avoidStruts $ layoutHook defaultConfig
+
+myManageHook = composeAll [
+    manageDocks,
+    isFullscreen --> doFullFloat,
+    className =? "Mpv" --> doFloat,
+    manageHook defaultConfig
+ ]
+
+myHandleEventHook = fullscreenEventHook
+
+myTerminal = "alacritty"
 
 --Bind Mod to the Windows Key
 myModMask = mod4Mask
 
-myTerminal = "alacritty"
+myWorkspaces = [ "1", "2", "3", "4", "5", "6", "7", "8", "9" ]
+
