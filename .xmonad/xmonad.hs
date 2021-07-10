5421c95252e45f72d8383983e58669c97b22d5e7
Akhilesh Kumar <rajakhil132@gmail.com>
2021-07-10 20:27:09 +0530

    Removing tree select action and its dependencies

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -25,7 +25,6 @@ import XMonad.Actions.CycleWS
 import XMonad.Actions.CycleRecentWS
 import XMonad.Actions.CycleWindows
 import qualified XMonad.Actions.FlexibleResize as Flex
-import qualified XMonad.Actions.TreeSelect as TS
 import XMonad.Prompt
 import XMonad.Prompt.FuzzyMatch
 import XMonad.Prompt.ConfirmPrompt
@@ -33,9 +32,6 @@ import XMonad.Prompt.Shell
 import XMonad.Prompt.Window
 import qualified  XMonad.StackSet as W
 import XMonad.Wallpaper
-import Data.Monoid
-import Data.Tree
-import qualified Data.Map as M
 
 
 main = do
@@ -75,7 +71,7 @@ myLayoutHook =  onWorkspace "1"
                 ( fullLayout
                   ||| tallLayout 
                 )
-              $ onWorkspace "3" floatingLayout
+              $ onWorkspace "5" floatingLayout
               $ tallLayout ||| fullLayout
  
 tallLayout = renamed [Replace "Tall"]
@@ -152,99 +148,6 @@ myXPConfig = def
   , sorter            = fuzzySort
   }
 
-treeselectAction a = TS.treeselectAction a
-   [ Node (TS.TSNode "+ Sound" "Adjust system audio" (return ())) 
-      [ Node (TS.TSNode "Toggle Mute" "Toogle Mute" (spawn toggleSoundMuteCmd)) []
-      , Node (TS.TSNode "90%" "90% Volume" (soundVolume 90)) []
-      , Node (TS.TSNode "80%" "80% Volume" (soundVolume 80)) []
-      , Node (TS.TSNode "70%" "70% Volume" (soundVolume 70)) []
-      , Node (TS.TSNode "60%" "60% Volume" (soundVolume 60)) []
-      , Node (TS.TSNode "50%" "50% Volume" (soundVolume 50)) []
-      , Node (TS.TSNode "40%" "40% Volume" (soundVolume 40)) []
-      , Node (TS.TSNode "30%" "30% Volume" (soundVolume 30)) []
-      , Node (TS.TSNode "20%" "20% Volume" (soundVolume 20)) []
-      ]
-   , Node (TS.TSNode "+ Microphone" "Adjust system microphone" (return ())) 
-      [ Node (TS.TSNode "Toggle Mute" "Toogle Mute" (spawn toggleMicrophoneMuteCmd)) []
-      , Node (TS.TSNode "90%" "90% Volume" (microphoneVolume 90)) []
-      , Node (TS.TSNode "80%" "80% Volume" (microphoneVolume 80)) []
-      , Node (TS.TSNode "70%" "70% Volume" (microphoneVolume 70)) []
-      , Node (TS.TSNode "60%" "60% Volume" (microphoneVolume 60)) []
-      , Node (TS.TSNode "50%" "50% Volume" (microphoneVolume 50)) []
-      , Node (TS.TSNode "40%" "40% Volume" (microphoneVolume 40)) []
-      , Node (TS.TSNode "30%" "30% Volume" (microphoneVolume 30)) []
-      , Node (TS.TSNode "20%" "20% Volume" (microphoneVolume 20)) []
-      ]
-   , Node (TS.TSNode "+ Brightness" "Adjust system brightness" (return ())) 
-      [ Node (TS.TSNode "Full" "Full Brightness" (brightness 0.99)) []
-      , Node (TS.TSNode "45%" "45% Brightness" (brightness 0.45)) []
-      , Node (TS.TSNode "50%" "50% Brightness" (brightness 0.50)) []
-      , Node (TS.TSNode "55%" "55% Brightness" (brightness 0.55)) []
-      , Node (TS.TSNode "60%" "60% Brightness" (brightness 0.60)) []
-      , Node (TS.TSNode "65%" "65% Brightness" (brightness 0.65)) []
-      , Node (TS.TSNode "70%" "70% Brightness" (brightness 0.70)) []
-      , Node (TS.TSNode "80%" "80% Brightness" (brightness 0.80)) []
-      , Node (TS.TSNode "90%" "90% Brightness" (brightness 0.90)) []
-      ]
-   , Node (TS.TSNode "+ Color Temperature" "Adjust color temperature" (return ())) 
-      [ Node (TS.TSNode "3000 K" "3000k temperature" (colorTemperature 3000)) []
-      , Node (TS.TSNode "3500 K" "3500k temperature" (colorTemperature 3500)) []
-      , Node (TS.TSNode "4000 K" "4000k temperature" (colorTemperature 4000)) []
-      , Node (TS.TSNode "4500 K" "4500k temperature" (colorTemperature 4500)) []
-      , Node (TS.TSNode "5000 K" "5000k temperature" (colorTemperature 5000)) []
-      , Node (TS.TSNode "6000K" "6000k temperature" (colorTemperature 6000)) [] 
-      , Node (TS.TSNode "10000K" "10000k temperature" (colorTemperature 10000)) []
-      , Node (TS.TSNode "15000K" "15000k temperature" (colorTemperature 15000)) []
-      , Node (TS.TSNode "20000K" "20000k temperature" (colorTemperature 20000)) []
-      , Node (TS.TSNode "25000K" "25000k temperature" (colorTemperature 25000)) []
-      ]
-   , Node (TS.TSNode "Shutdown" "Poweroff" (spawn "poweroff")) []
-   , Node (TS.TSNode "Reboot"   "Reboot"   (spawn "reboot")) []
-   ]
-
-toggleSoundMuteCmd = "amixer -q sset Master toggle"
-toggleMicrophoneMuteCmd = "amixer -q sset Capture toggle"
-
-soundVolume percentage = spawn ("amixer -M set Master " ++ show percentage ++ "%")
-microphoneVolume percentage = spawn ("amixer -M set Capture " ++ show percentage ++ "%")
-
-brightness percentage = spawn ("redshift -P -o -l 24:84 -b " ++ show percentage)
-colorTemperature temperature =   spawn ("redshift -P -l 24:84 -O "++ show temperature ++ "k") 
-
-tsDefaultConfig = TS.TSConfig { TS.ts_hidechildren = True
-                              , TS.ts_background   = 0xdd282c34
-                              , TS.ts_font         = "xft:Sans-10"
-                              , TS.ts_node         = (0xffd0d0d0, 0xff1c1f24)
-                              , TS.ts_nodealt      = (0xffd0d0d0, 0xff282c34)
-                              , TS.ts_highlight    = (0xffffffff, 0xff755999)
-                              , TS.ts_extra        = 0xffd0d0d0
-                              , TS.ts_node_width   = 200
-                              , TS.ts_node_height  = 20
-                              , TS.ts_originX      = 100
-                              , TS.ts_originY      = 100
-                              , TS.ts_indent       = 80
-                              , TS.ts_navigate     = myTreeNavigation
-                              }
-
-
-myTreeNavigation = M.fromList
-    [ ((0, xK_Escape), TS.cancel)
-    , ((0, xK_Return), TS.select)
-    , ((0, xK_space),  TS.select)
-    , ((0, xK_Up),     TS.movePrev)
-    , ((0, xK_Down),   TS.moveNext)
-    , ((0, xK_Left),   TS.moveParent)
-    , ((0, xK_Right),  TS.moveChild)
-    , ((0, xK_k),      TS.movePrev)
-    , ((0, xK_j),      TS.moveNext)
-    , ((0, xK_h),      TS.moveParent)
-    , ((0, xK_l),      TS.moveChild)
-    , ((0, xK_o),      TS.moveHistBack)
-    , ((0, xK_i),      TS.moveHistForward)
-    , ((0, xK_s),      TS.moveTo ["Shutdown"])
-    ]
-
-
 myAdditionalKeysP = [
   ("M-<Return>", promote),
   -- Replace dmenu with rofi
@@ -265,9 +168,9 @@ myAdditionalKeysP = [
   ("M-S-a" , kill1),
   ("M1-<Tab>"   , cycleRecentWindows [xK_Alt_L] xK_Tab xK_Tab ),
   -- Workspace bindings
-  ("M-<Left>", moveTo Prev NonEmptyWS),
-  ("M-<Right>", moveTo Next NonEmptyWS),
-  ("M-<Tab>"   , cycleRecentWS [xK_Super_L] xK_Tab xK_Tab ),
+  ("M-<Left>"    , moveTo Prev NonEmptyWS),
+  ("M-<Right>"   , moveTo Next NonEmptyWS),
+  ("M-<Tab>"     , cycleRecentWS [xK_Super_L] xK_Tab xK_Tab ),
   -- Prompts keybindings
   ("M-S-p", shellPrompt myXPConfig),
   ("M-s a", promptSearch myXPConfig amazon),
@@ -278,19 +181,18 @@ myAdditionalKeysP = [
   ("M-s w", promptSearch myXPConfig wikipedia),
   ("M-s y", promptSearch myXPConfig youtube),
   -- Utilities and extensions
-  ("M-x t", treeselectAction tsDefaultConfig),
-  ("M-<Backspace>", spawn "sh /home/akhilesh/.repo/styli.sh/styli.sh -w 1920 -h 1080"),
+  ("M-<Backspace>", spawn "sh $HOME/.repo/styli.sh/styli.sh -w 1920 -h 1080"),
   ("M-<Escape>", spawn "i3lock-color -c 222222"),
  -- Multimedia key bindings
  ("M-<Up>",                     spawn ("amixer set Master 5%+")),
- ("<XF86AudioRaiseVolume>",     spawn ("amixer set Master 5%+")),
- ("<XF86AudioMute>",            spawn ("amixer -q sset Master toggle")),
  ("M-<Down>",                   spawn ("amixer set Master 5%-")),
- ("<XF86AudioLowerVolume>",     spawn ("amixer set Master 5%-")),
  ("M-S-<Up>",                   spawn ("amixer set Capture 10%+")),
  ("M-S-<Down>",                 spawn ("amixer set Capture 10%-")),
- ("M-C-<Up>",                   spawn ("xbrightness +1000")),
- ("M-C-<Down>",                 spawn ("xbrightness -1000")),
+ ("M-C-<Up>",                   spawn ("xbrightness +1500")),
+ ("M-C-<Down>",                 spawn ("xbrightness -1500")),
+ ("<XF86AudioLowerVolume>",     spawn ("amixer set Master 5%-")),
+ ("<XF86AudioRaiseVolume>",     spawn ("amixer set Master 5%+")),
+ ("<XF86AudioMute>",            spawn ("amixer -q sset Master toggle")),
  ("<XF86AudioPlay>",     spawn ("playerctl play-pause")),
  ("<XF86AudioStop>",     spawn ("playerctl stop")),
  ("<XF86AudioPrev>",     spawn ("playerctl previous")),
